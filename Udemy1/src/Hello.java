c747a8764999c5e8cb1e17e67f6efb63a5dc62fc
thomwise <thomas.ja.wise@gmail.com>
2018-11-02 23:43:27 -0700

    Udemy Practice

---
+++ Udemy1/src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Thom!");
+
+        int myFirstNumber = 7;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int newTotal = 1000 - myTotal;
+
+        System.out.println(newTotal);
+    }
+
+}
