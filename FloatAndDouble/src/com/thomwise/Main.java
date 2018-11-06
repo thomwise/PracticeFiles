7a9a998e4fa3755b20cb93d63f30a4be5765f88a
thomwise <thomas.ja.wise@gmail.com>
2018-11-06 05:25:52 -0800

    float and double practice

---
+++ FloatAndDouble/src/com/thomwise/Main.java
@@ -0,0 +1,21 @@
+package com.thomwise;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double pounds = 200;
+        double toKilos = pounds * 0.45359237;
+
+        System.out.println("Kilograms = " + toKilos);
+
+        int myIntValue = 5 / 2;
+        float myFloatValue = 5f / 3;
+        double myDoubleValue = 5d / 3;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+
+    }
+}
