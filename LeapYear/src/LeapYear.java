300d245616b546f1e5890e2f10e0479e803eb7b7
thomwise <thomas.ja.wise@gmail.com>
2018-11-16 09:03:36 -0800

    For Loop Practice

--- LeapYear/src/LeapYear.java
+++ LeapYear/src/LeapYear.java
@@ -1,4 +1,9 @@
 public class LeapYear {
+
+    public static void main(String[] args) {
+        System.out.println(getDaysInMonth(12, 10050));
+    }
+
     public static boolean isLeapYear(int year) {
 
         if (year < 1 || year > 9999) {
@@ -8,4 +13,28 @@ public class LeapYear {
         } else
             return false;
     }
-}C
+
+    public static int getDaysInMonth(int month, int year) {
+
+        if((month < 1 || month > 12) || (year < 1 || year > 9999)) {
+            return -1;
+        } else {
+            switch(month) {
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                    return 31;
+
+                case 4: case 6: case 9: case 11:
+                    return 30;
+
+                case 2:
+                    if(isLeapYear(year)) {
+                        return 29;
+                    } else
+                        return 28;
+            }
+            return -1;
+        }
+
+    }
+
+}
