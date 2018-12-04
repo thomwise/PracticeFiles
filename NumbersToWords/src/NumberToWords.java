0d76755673deea023051e8c877fbba9e6889063a
thomwise <thomas.ja.wise@gmail.com>
2018-12-04 13:46:30 -0800

    Parsing Strings to Intergers

--- NumbersToWords/src/NumberToWords.java
+++ NumbersToWords/src/NumberToWords.java
@@ -3,70 +3,96 @@ package PACKAGE_NAME;
 public class NumberToWords {
 
     public static void main(String[] args) {
-        numberToWords(4097);
+        numberToWords(100000);
+
+
     }
 
     public static void numberToWords(int number) {
 
-        if(number < 0) {
+        if (number < 0) {
             System.out.println("Invalid Value");
-        } else {
+        }
 
-            int reverseNum = reverse(number);
-            int count = 0;
 
-            while(reverseNum > 0) {
+        int reverse = reverse(number);
+        int reverse2 = reverse;
 
-                int lastDigit = reverseNum % 10;
-                switch (lastDigit) {
-                    case 0:
-                        System.out.println("Zero");
-                        break;
-                    case 1:
-                        System.out.println("One");
-                        break;
-                    case 2:
-                        System.out.println("Two");
-                        break;
-                    case 3:
-                        System.out.println("Three");
-                        break;
-                    case 4:
-                        System.out.println("Four");
-                        break;
-                    case 5:
-                        System.out.println("Five");
-                        break;
-                    case 6:
-                        System.out.println("Six");
-                        break;
-                    case 7:
-                        System.out.println("Seven");
-                        break;
-                    case 8:
-                        System.out.println("Eight");
-                        break;
-                    case 9:
-                        System.out.println("Nine");
-                        break;
-                }
-                count += 1;
+        int lastDigit = 0;
 
+        while (reverse > 0) {
+            lastDigit = reverse % 10;
+
+            switch (lastDigit) {
+                case 0:
+                    System.out.println("Zero");
+                    break;
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Five");
+                    break;
+                case 6:
+                    System.out.println("Six");
+                    break;
+                case 7:
+                    System.out.println("Seven");
+                    break;
+                case 8:
+                    System.out.println("Eight");
+                    break;
+                case 9:
+                    System.out.println("Nine");
+                    break;
             }
+            reverse /= 10;
+
+        }
+
+        while(getDigitCount(number) != getDigitCount(reverse2)) {
+            System.out.println("Zero");
+            reverse2 = reverse2 * 10;
         }
 
     }
 
     public static int reverse(int number) {
-
         int reverse = 0;
-        int lastDigit = number%10;
         while(number != 0) {
-            reverse = (reverse*10) + lastDigit;
-            reverse /= 10;
-            continue;
+            int lastDigit = number%10;
+            reverse = (reverse * 10) + lastDigit;
+            number = number / 10;
+
         }
         return reverse;
     }
 
+    public static int getDigitCount(int number) {
+
+        if(number == 0) {
+            return 1;
+        } else if(number < 0) {
+            return -1;
+        }
+
+        int counter = 0;
+
+        while (number != 0) {
+            number /= 10;
+            counter++;
+
+        }
+        return counter;
+
+    }
 }
