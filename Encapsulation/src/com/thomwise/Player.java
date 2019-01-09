87e9eac4cd948412969ac64cfd3edc0e4f1ed580
thomwise <thomas.ja.wise@gmail.com>
2019-01-09 09:05:38 -0800

    Learning encapsulation

---
+++ Encapsulation/src/com/thomwise/Player.java
@@ -0,0 +1,22 @@
+package com.thomwise;
+
+public class Player {
+
+    public String name;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage) {
+        this.health = this.health - damage;
+
+        if(this.health <= 0) {
+            System.out.println("Player knocked out");
+            //Reduce number of lives for player
+        }
+    }
+
+    public int healthRemaining() {
+        return this.health;
+    }
+
+}
