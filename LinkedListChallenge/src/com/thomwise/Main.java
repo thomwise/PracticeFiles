888bd5dfa41e4d92c7b80d9e31d8a9594d346a83
System Administrator <root@Rachel-Kennys-MacBook-Pro.local>
2019-04-05 11:39:36 -0700

    Finished linked list project

--- LinkedListChallenge/src/com/thomwise/Main.java
+++ LinkedListChallenge/src/com/thomwise/Main.java
@@ -100,12 +100,16 @@ public class Main {
                     if(forward) {
                         if(listIterator.hasPrevious()) {
                             System.out.println("Now replaying " + listIterator.previous().toString());
+                            forward = false;
                         } else {
                             System.out.println("We are at the start of the list.");
                         }
                     } else {
                         if(listIterator.hasNext()) {
                             System.out.println("Now replaying " + listIterator.next().toString());
+                            forward = true;
+                        } else{
+                            System.out.println("We have reached the end of the list.");
                         }
                     }
                     break;
@@ -115,6 +119,15 @@ public class Main {
                 case 5:
                     printMenu();
                     break;
+                case 6:
+                    if(playlist.size() > 0) {
+                        listIterator.remove();
+                        if(listIterator.hasNext()) {
+                            System.out.println("Now playing " + listIterator.next());
+                        } else if(listIterator.hasPrevious()) {
+                            System.out.println("Now playing " + listIterator.previous());
+                        }
+                    }
             }
         }
     }
@@ -126,7 +139,8 @@ public class Main {
                         "2 - to play previous song\n" +
                         "3 - to replay the current song\n" +
                         "4 - list songs in the playlist\n" +
-                        "5 - print available actions");
+                        "5 - print available actions" +
+                        "6 - delete current song from playlist");
     }
 
     private static void printList(LinkedList<Song> playlist) {
